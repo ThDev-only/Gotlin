b5ee96fb5eba8076d526d0b744f6591126111585
ThDev-only <rthiarley@gmail.com>
2025-01-19 01:46:52 -0300

    fix: dir archives go

--- app/src/main/java/com/thdev/gotlin/MainActivity.kt
+++ app/src/main/java/com/thdev/gotlin/MainActivity.kt
@@ -3,24 +3,15 @@ package com.thdev.gotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.thdev.gotlin.databinding.ActivityMainBinding
 
 public class MainActivity : AppCompatActivity() {
 
-    private var binding: ActivityMainBinding? = null
-    
-    private val binding: ActivityMainBinding
-      get() = checkNotNull(binding) { "destroyed" }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setContentView(binding.root)
     }
     
     override fun onDestroy() {
         super.onDestroy()
-        binding = null
     }
 }
